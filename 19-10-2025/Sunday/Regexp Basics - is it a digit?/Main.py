e4d51a8a73b72463d2a0119f45f23f18a77098a1
Ayoni02 <ayoni.sakibu@gmail.com>
2025-11-01 18:50:54 +0100

    Initial commit

--- 19-10-2025/Sunday/Regexp Basics - is it a digit?/Main.py
+++
@@ -1,9 +0,0 @@
-def is_digit(n):
-    #your code here
-    if len(n) > 1:
-        return False
-    try:
-        int(n)
-        return True
-    except:
-        return False
---
+++ 3rd-10-2025/Thursday/Automorphic Number/Main.py
@@ -0,0 +1,4 @@
+def automorphic(num):
+    if (str(num*num)).endswith(str(num)):
+        return "Automorphic"
+    return "Not!!"
